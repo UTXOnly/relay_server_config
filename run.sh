1c0ceb61df93eb4f65737952f03d68cff5667b15
Brian Hartford <bhartford419@gmail.com>
2023-01-14 15:53:49 -0500

    Add nginx escap char

--- run.sh
+++ run.sh
@@ -36,16 +36,16 @@ sudo tee /etc/nginx/sites-available/default <<EOF
 server{
     server_name ${domain_name};
     location / {
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header Host $host;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header Host \$host;
         proxy_pass http://127.0.0.1:8008;
         proxy_http_version 1.1;
-        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection "upgrade";
     }
 }
 EOF
-sudo chown ${UID} /etc/nginx/sites-available/default
+sudo chown $UNAME:$UNAME /etc/nginx/sites-available/default
 # Restart nginx
 sudo service nginx restart
 
