f502e645fb0fc659bb2b91aa0d215cbe1dcf5e85
Brian Hartford <bhartford419@gmail.com>
2023-01-14 15:02:05 -0500

    fixed ngninx config errors

--- run.sh
+++ run.sh
@@ -36,11 +36,11 @@ sudo tee /etc/nginx/sites-available/default <<EOF
 server{
     server_name ${domain_name};
     location / {
-        proxy_set_header X-Forwarded-For ${proxy_add_x_forwarded_for};
-        proxy_set_header Host ${HOSTNAME};
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header Host $host;
         proxy_pass http://127.0.0.1:8008;
         proxy_http_version 1.1;
-        proxy_set_header Upgrade ${http_upgrade};
+        proxy_set_header Upgrade $http_upgrade;
         proxy_set_header Connection "upgrade";
     }
 }
